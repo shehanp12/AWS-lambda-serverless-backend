8acb3db8188206885765d3926e8de2453a20a07a
Shehan Prasanna <shehanp068@gmail.com>
2021-04-26 01:19:54 +0530

    dynamo db configrations

--- handler.js
+++ handler.js
@@ -1,5 +1,9 @@
 const awsServerlessExpress = require("aws-serverless-express");
+const AWS = require('aws-sdk');
 const app = require("src/index");
+const db = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
+const uuid = require('uuid/v4');
+
 
 const server= awsServerlessExpress.createServer(app);
 
